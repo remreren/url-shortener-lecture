1c6115894ed2b16cf172e3edae018e6cb65da958
remreren <eeren881@gmail.com>
2023-03-26 23:04:01 +0300

    spring boot app added

---
+++ url-shortener-app/src/test/java/com/remreren/urlshortenerapp/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remreren.urlshortenerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
